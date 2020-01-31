4efa84bbaca9640da32c6d4d15e61bb1a3e738e2
Rkiguel Figueiro <riguel.figueiro@thoughtworks.com>
2020-01-30 23:42:45 -0300

    created logout test

--- src/app/services/login/login.service.spec.ts
+++ src/app/services/login/login.service.spec.ts
@@ -4,21 +4,53 @@ import { LoginService } from './login.service';
 import { HttpClientModule } from '@angular/common/http';
 
 describe('LoginService', () => {
-  beforeEach(() =>
-    TestBed.configureTestingModule({ imports: [HttpClientModule] })
-  );
+  function createLocalStorageMock() {
+    let store = {};
+    const mockLocalStorage = {
+      getItem: (key: string): string => {
+        return key in store ? store[key] : null;
+      },
+      setItem: (key: string, value: string) => {
+        store[key] = `${value}`;
+      },
+      removeItem: (key: string) => {
+        delete store[key];
+      },
+      clear: () => {
+        store = {};
+      }
+    };
+
+    spyOn(localStorage, 'getItem').and.callFake(mockLocalStorage.getItem);
+    spyOn(localStorage, 'setItem').and.callFake(mockLocalStorage.setItem);
+    spyOn(localStorage, 'removeItem').and.callFake(mockLocalStorage.removeItem);
+  }
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({ imports: [HttpClientModule] });
+    createLocalStorageMock();
+  });
 
   it('should be created', () => {
     const service: LoginService = TestBed.get(LoginService);
     expect(service).toBeTruthy();
   });
 
-  it('should create a new user', () => {
+  it('should todo login', () => {
     const email = 'fakeUser@gmail.com';
     const password = '123456';
     const service: LoginService = TestBed.get(LoginService);
     const isSuccess = service.login(email, password);
 
     expect(isSuccess).toBeTruthy();
+  });
+
+  it('should todo logout', () => {
+    localStorage.setItem('currentUser', 'fakeCurrentUser');
+    const service: LoginService = TestBed.get(LoginService);
+
+    service.logout();
+
+    expect(localStorage.getItem('currentUser')).toBeNull();
   });
 });
