cf52693669eda2d982b7b76df10a0a873d388a42
Rkiguel Figueiro <riguel.figueiro@thoughtworks.com>
2020-01-31 17:41:02 -0300

    added config file for api endpoints

---
+++ src/app/config/index.ts
@@ -0,0 +1,3 @@
+export const BASE_URL_MOVIES = 'https://api.themoviedb.org/3';
+export const BASE_URL_IMAGES = 'https://image.tmdb.org/t/p/original/';
+export const API_KEY = '2590ac2e9a87fbfe0e649b19ae1b91c2';
